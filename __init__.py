acb27a0600feb942872aa86ae1cb5d4f0515c649
YiboW <yibocat@yeah.net>
2023-01-29 14:34:21 +0800

    update __init__.py

---
+++ __init__.py
@@ -0,0 +1,26 @@
+#  Copyright (c) yibocat 2023 All Rights Reserved
+#  Date: 2023/1/29 下午2:18
+#  Author: yibow
+#  Email: yibocat@yeah.net
+#  Software: FuzzyKit
+
+__all__ = ['HQrungF', 'Intersection', 'Union',
+           'algebraicMultiplication', 'algebraicPlus',
+           'einsteinMultiplication', 'einsteinPlus',
+           'QrungFN', 'Intersection', 'Union',
+           'algebraicMultiply', 'algebraicPlus',
+           'einsteinMultiply', 'einsteinPlus',
+           'QrungIVFN',
+           'algebraicMultiply', 'algebraicPlus',
+           'einsteinMultiply', 'einsteinPlus'
+           ]
+
+from DHFElements import (HQrungF, Intersection, Union,
+                         algebraicMultiplication, algebraicPlus,
+                         einsteinMultiplication, einsteinPlus)
+from FNumbers import (QrungFN, Intersection, Union,
+                      algebraicMultiply, algebraicPlus,
+                      einsteinMultiply, einsteinPlus)
+from IVFNumbers import (QrungIVFN,
+                        algebraicMultiply, algebraicPlus,
+                        einsteinMultiply, einsteinPlus)
